024b506f20409c0995949a4f597aba7455b7b1e7
Gregory <56291043+GregoryBartolo@users.noreply.github.com>
2021-12-30 15:29:12 +0100

    finish part 5

--- part1-analyse-exploratoire.py
+++ part1-analyse-exploratoire.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
 
-df = pd.read_csv("../Catalogue.csv", encoding='latin-1')
 
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
@@ -9,24 +10,65 @@ pd.set_option('display.width', 1000)
 pd.set_option('display.colheader_justify', 'center')
 pd.set_option('display.precision', 3)
 
-# Display a describe of all columns in current df, could be splited as 
-# include=[np.number] / include=[object] / include=['col']
-print(df.describe(include='all'))
 
-df_analyse = pd.DataFrame()
+def analyseCSV(name, listDim, parallel_color, yVio, xVio, colorVio):
 
-ser = []
-# For each column in df, append to a Pandas Series a key/value association which contains a counts value for elems 
-[ser.append(str({k:v for k,v in zip(df[col].value_counts().index,df[col].value_counts().values)})) for col in df.columns]
-# Add the Pandas Series to df_analyse with good mapping indexes
-df_analyse = pd.concat([df_analyse, pd.Series(ser, index=df.columns).to_frame().T])
-# Rename row index in df_analyse
-df_analyse.rename(index={0:'Count'},inplace=True)
+    df = pd.read_csv("../"+name+".csv", encoding='latin-1')
 
-# Same shit with normalize which convert count to percentage, to provide more analytics ways
-ser = []
-[ser.append(str({k:v for k,v in zip(df[col].value_counts().index,df[col].value_counts(normalize=True).values)})) for col in df.columns]
-df_analyse = pd.concat([df_analyse, pd.Series(ser, index=df.columns).to_frame().T])
-df_analyse.rename(index={0:'Percent'},inplace=True)
+    # Display a describe of all columns in current df, could be splited as 
+    # include=[np.number] / include=[object] / include=['col']
+    print(" *********** " + name + " Describe ***********")
+    print(df.describe(include='all'))
 
-print(df_analyse)
+
+    print(" *********** " + name + " Infos ***********")
+    print(df.info())
+
+    print(" *********** " + name + " Missing Values ***********")
+    print(df.isna().sum())
+
+    print(" *********** " + name + " Alluvial Diagram")
+    fig = px.parallel_categories(df, dimensions=listDim,
+                    color=parallel_color, color_continuous_scale=px.colors.sequential.Inferno)
+    fig.show()
+
+    #fig = px.parallel_categories(df[['marque', 'nom', 'puissance', 'longueur', 'nbPlaces', 'nbPortes', 'couleur', 'occasion', 'prix']],
+    #color_continuous_scale=px.colors.sequential.Inferno)
+    #fig.show()
+
+    print(" *********** " + name + " Violon Diagram")
+    fig = px.violin(df, y=yVio, x=xVio, box=True, color=colorVio, points='all', hover_data=df.columns)
+    fig.show()
+
+
+    df_analyse = pd.DataFrame()
+
+    ser = []
+    # For each column in df, append to a Pandas Series a key/value association which contains a counts value for elems 
+    [ser.append(str({k:v for k,v in zip(df[col].value_counts().index,df[col].value_counts().values)})) for col in df.columns]
+    # Add the Pandas Series to df_analyse with good mapping indexes
+    df_analyse = pd.concat([df_analyse, pd.Series(ser, index=df.columns).to_frame().T])
+    # Rename row index in df_analyse
+    df_analyse.rename(index={0:'Count'},inplace=True)
+
+    # Same thing with normalize which convert count to percentage, to provide more analytics ways
+    ser = []
+    [ser.append(str({k:v for k,v in zip(df[col].value_counts().index,df[col].value_counts(normalize=True).values)})) for col in df.columns]
+    df_analyse = pd.concat([df_analyse, pd.Series(ser, index=df.columns).to_frame().T])
+    df_analyse.rename(index={0:'Percent'},inplace=True)
+
+    print(df_analyse)
+
+    # Duplicated rows ? 
+    #[print(x) for x in df.duplicated()]
+
+
+listCatalogue = ['marque', 'nom', 'puissance', 'longueur', 'nbPlaces', 'nbPortes', 'couleur', 'occasion']
+analyseCSV('Catalogue',listCatalogue, 'prix', 'prix', 'marque', 'marque')
+
+
+#age,sexe,taux,situationFamiliale,nbEnfantsAcharge,voitureSecondaire
+#21,F,1396,Cï¿½libataire,0,false
+
+listMarketing = ['age','sexe','taux','situationFamiliale','nbEnfantsAcharge','voitureSecondaire']
+analyseCSV('Marketing',listMarketing, 'taux', 'taux', 'situationFamiliale', 'situationFamiliale')
--- part5-classification-supervisee.py
+++ part5-classification-supervisee.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
 
 clients_and_immatriculations = pd.read_csv("Clients_Immatriculations.csv", encoding='latin-1')
 # print(clients_and_immatriculations.columns)
@@ -10,21 +12,134 @@ noms_categorie = clients_and_immatriculations['Categorie'].unique()
 nombres_vehicule = clients_and_immatriculations.groupby(clients_and_immatriculations['Categorie'], sort = False).size()
 # print(nombres_vehicule)
 
-# plt.bar(noms_categorie, nombres_vehicule, color=['r','blue','orange','y'])
-# plt.title('Diagramme: Categorie vs Nombre')
-# plt.xlabel('Nom des categories')
-# plt.ylabel('Nombre')
-# plt.show()
+# Nettoyage des donnees
+########################################################################
+clients_and_immatriculations['sexe'].replace({
+        'Féminin': 'F',
+        'Femme': 'F',
+        'Homme': 'M',
+        'Masculin': 'M',
+        'N/D': '?',
+        ' ': '?',
+    }, inplace=True)
 
-x = clients_and_immatriculations.iloc[:,[0]].values
+clients_and_immatriculations['situationFamiliale'].replace({
+        'Seule': 'Célibataire',
+        'Seul': 'Célibataire',
+        'Divorcée': 'Célibataire',
+        'Marié(e)': 'En Couple',
+        ' ': '?',
+        'N/D': '?',
+    }, inplace=True)
+
+clients_and_immatriculations['age'].replace({
+        '-1': '?',
+        ' ': '?',
+    }, inplace=True)
+
+clients_and_immatriculations['taux'].replace({
+        '-1': '?',
+        ' ': '?',
+    }, inplace=True)
+
+clients_and_immatriculations['nbEnfantsAcharge'].replace({
+        '-1': '?',
+        ' ': '?',
+    }, inplace=True)
+
+clients_and_immatriculations['2eme voiture'].replace({
+        ' ': '?',
+    }, inplace=True)
+
+clients_and_immatriculations = clients_and_immatriculations[clients_and_immatriculations['age'] != '?']
+clients_and_immatriculations = clients_and_immatriculations[clients_and_immatriculations['sexe'] != '?']
+clients_and_immatriculations = clients_and_immatriculations[clients_and_immatriculations['situationFamiliale'] != '?']
+clients_and_immatriculations = clients_and_immatriculations[clients_and_immatriculations['2eme voiture'] != '?']
+clients_and_immatriculations = clients_and_immatriculations[clients_and_immatriculations['taux'] != '?']
+clients_and_immatriculations = clients_and_immatriculations[clients_and_immatriculations['nbEnfantsAcharge'] != '?']
+########################################################################
+
+# Changement des donnees string en numerique
+########################################################################
+# 1 pour Homme et 0 pour Femme
+clients_and_immatriculations.loc[(clients_and_immatriculations.sexe == 'M'),'sexe'] = '1'
+clients_and_immatriculations.loc[(clients_and_immatriculations.sexe == 'F'),'sexe'] = '0'
+
+# 1 pour En Couple et 0 pour Celibataire
+clients_and_immatriculations.loc[(clients_and_immatriculations.situationFamiliale == 'En Couple'),'situationFamiliale'] = '1'
+clients_and_immatriculations.loc[(clients_and_immatriculations.situationFamiliale == 'Célibataire'),'situationFamiliale'] = '0'
+
+# 1 pour true et 0 pour false
+clients_and_immatriculations.loc[(clients_and_immatriculations['2eme voiture'] == 'true'),'2eme voiture'] = '1'
+clients_and_immatriculations.loc[(clients_and_immatriculations['2eme voiture'] == 'false'),'2eme voiture'] = '0'
+########################################################################
+
+clients_and_immatriculations["age"] = pd.to_numeric(clients_and_immatriculations["age"])
+clients_and_immatriculations["sexe"] = pd.to_numeric(clients_and_immatriculations["sexe"])
+clients_and_immatriculations["situationFamiliale"] = pd.to_numeric(clients_and_immatriculations["situationFamiliale"])
+clients_and_immatriculations["taux"] = pd.to_numeric(clients_and_immatriculations["taux"])
+clients_and_immatriculations["nbEnfantsAcharge"] = pd.to_numeric(clients_and_immatriculations["nbEnfantsAcharge"])
+clients_and_immatriculations["2eme voiture"] = pd.to_numeric(clients_and_immatriculations["2eme voiture"])
+
+
+x = clients_and_immatriculations.iloc[:,[0,1,2,3,4,5]].values
 y = clients_and_immatriculations.iloc[:,16].values
-# print(x,y)
 
-from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x,y,random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(x,y,random_state = 0)
 
-from sklearn.preprocessing import MinMaxScaler
+# Decision Tree
+############################################################################
+from sklearn.tree import DecisionTreeClassifier
+clf = DecisionTreeClassifier().fit(X_train, y_train)
+print('Accuracy of Decision Tree classifier on training set: {:.2f}'
+     .format(clf.score(X_train, y_train)))
+print('Accuracy of Decision Tree classifier on test set: {:.2f}'
+     .format(clf.score(X_test, y_test)))
+############################################################################
 
-scaler = MinMaxScaler()
-x_train = scaler.fit_transform(x_train)
-x_test = scaler.transform(x_test)
+# K-Nearest Neighbors
+############################################################################
+print()
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier()
+knn.fit(X_train, y_train)
+print('Accuracy of K-NN classifier on training set: {:.2f}'
+     .format(knn.score(X_train, y_train)))
+print('Accuracy of K-NN classifier on test set: {:.2f}'
+     .format(knn.score(X_test, y_test)))
+############################################################################
+
+# Linear Discriminant Analysis
+############################################################################
+print()
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+lda = LinearDiscriminantAnalysis()
+lda.fit(X_train, y_train)
+print('Accuracy of LDA classifier on training set: {:.2f}'
+     .format(lda.score(X_train, y_train)))
+print('Accuracy of LDA classifier on test set: {:.2f}'
+     .format(lda.score(X_test, y_test)))
+############################################################################
+
+# Gaussian Naive Bayes
+############################################################################
+print()
+from sklearn.naive_bayes import GaussianNB
+gnb = GaussianNB()
+gnb.fit(X_train, y_train)
+print('Accuracy of GNB classifier on training set: {:.2f}'
+     .format(gnb.score(X_train, y_train)))
+print('Accuracy of GNB classifier on test set: {:.2f}'
+     .format(gnb.score(X_test, y_test)))
+############################################################################
+
+# Matrice de confusion
+############################################################################
+print()
+print('Matrice de confusion :')
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+pred = knn.predict(X_test)
+print(confusion_matrix(y_test, pred))
+print(classification_report(y_test, pred))
+############################################################################
